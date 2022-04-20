5640a782285f475b72c5a2ed52088dfbc4b004bb
Brandon Hang <brandonhang34@outlook.com>
2022-04-20 14:44:57 -0700

    Lab 3 finished

--- class_grade.py
+++ class_grade.py
@@ -2,7 +2,6 @@
 
 
 
-
 ''' PLAIN ENGLISH
 start
 Create a list to store 5 number (float)
@@ -55,13 +54,8 @@ grade = input("Enter the 5th grade: ")
 grades.append(float(grade))
 
 grades.sort()
-
 grades = grades[2:]
-
 grades = sum(grades)
-
 result = grades / 3
 
-print("Average Grade {0:2f}".format(result))
-
-print(grades, 'results', result)
+print("Average Grade {0:2f}%".format(result))
